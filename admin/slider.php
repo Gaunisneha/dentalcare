b680ff4eb46be9c91cfd847ed24b6625f7bbf3ad
Gaunisneha <snehagauni@gmail.com>
2025-02-03 21:19:17 +0530

    updated

--- admin/slider.php
+++ admin/slider.php
@@ -15,7 +15,7 @@
                     </div>
                 </div>
                 <div class="navbar-nav w-100">
-                    <a href="adminhome.php" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+                    <a href="adminhome.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
                     <div class="nav-item dropdown">
                         <!-- <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Elements</a> -->
                         <!-- <div class="dropdown-menu bg-transparent border-0">
@@ -24,10 +24,10 @@
                             <a href="element.html" class="dropdown-item">Other Elements</a>
                         </div> -->
                     </div>
-                    <a href="showdoctor.php" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Doctors</a>
-                    <a href="showregistration.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>registrations</a>
-                    <a href="showappointment.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>appointment</a>
-                    <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>payment</a>
+                    <a href="showdoctor.php" class="nav-item nav-link"><i class="fa fa-user-nurse"></i>Doctors</a>
+                    <a href="showregistration.php" class="nav-item nav-link"><i class="fa fa-users"></i>Patient</a>
+                    <a href="showappointment.php" class="nav-item nav-link"><i class="fa fa-calendar-check"></i>AppointmentS</a>
+                    <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Payment</a>
                     <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Feedback</a>
                     <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
                     <div class="nav-item dropdown">
